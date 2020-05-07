5341c6766856f3e94036317d99271386609f2a67
athos9494 <1026060156@qq.com>
2020-05-07 08:56:35 +0800

    .

--- 0506/cake/src/club/banyuan/Main.java
+++ 0506/cake/src/club/banyuan/Main.java
@@ -2,8 +2,11 @@ package club.banyuan;
 
 import java.util.concurrent.Callable;
 public class Main{
-    static Cake []cakes = new Cake[10];
+    public  static Cake[] CreateCake()
     {
+        
+        Cake []cakes = new Cake[10];
+        
         cakes[0] = new OrderCake(0,10,3);
         cakes[1] = new OrderCake(1,20,3);
         cakes[2] = new OrderCake(2,30,5);
@@ -14,20 +17,34 @@ public class Main{
         cakes[7] = new ReadyMadeCake(7,30,1);
         cakes[8] = new ReadyMadeCake(8,20,1);
         cakes[9] = new ReadyMadeCake(9,10,1);
-    }
-    public static double allPrice(){
+        return cakes;
 
-         return 0;
     }
-    public static double  maxPrice(){
+    public static void  allPrice(Cake []cakes){
+        double allp = 0;
+        for (int i = 0;i < 10 ;i++)
+        {
+            allp = cakes[i].calcPrice() + allp;
+        }
 
-        return 0;
+         System.out.println("所有蛋糕的总价是：" +allp);
     }
-    public static double allReadyMadeCakePrice(){
-        return 0;
+    public static void maxPrice(Cake []cakes){
+        double max = cakes[0].calcPrice();
+        for (int i = 0 ;i< 10 ;i++)
+        {
+            if (cakes[i].calcPrice()>max){
+                max = cakes[i].calcPrice();
+            }
+        }
+        System.out.println("蛋糕价格最高的是：" + max);
+
     }
-    public static double allReadyMadeCakecount(){
-        return 0;
+    public static void  allReadyMadeCakePrice(){
+        System.out.println("所有ReadyMadeCake的总价是:");
+    }
+    public static void allReadyMadeCakecount(){
+        System.out.println("所有ReadyMadeCake的数量是：");
     }
     public static void main(String[] args) {
         //Cake []cakes = new Cake[10];
@@ -42,12 +59,11 @@ public class Main{
         //cakes[8] = new ReadyMadeCake(8,20,1);
         //cakes[9] = new ReadyMadeCake(9,10,1);
 
-
-        System.out.println("所有蛋糕的总价是：" + allPrice());
-
-        System.out.println("ReadyMadeCake蛋糕的总价是：" + allReadyMadeCakePrice()+ "数量是："+allReadyMadeCakecount());
-
-        System.out.println("所有蛋糕中单价最高的蛋糕是："+ maxPrice());
+        Cake []cakes = CreateCake();
+        allPrice(cakes);
+        maxPrice(cakes);
+        allReadyMadeCakecount();
+        allReadyMadeCakePrice();
 
     }
 }
