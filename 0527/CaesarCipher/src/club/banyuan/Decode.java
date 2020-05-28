0b6150712e1cb37aba230e34f5813d62edad1c0f
athos9494 <1026060156@qq.com>
2020-05-28 08:43:40 +0800

    .

--- 0527/CaesarCipher/src/club/banyuan/Decode.java
+++ 0527/CaesarCipher/src/club/banyuan/Decode.java
@@ -1,14 +1,14 @@
 package club.banyuan;
 
 public class Decode {
-    public static void decode(String string){
+    public static void decode(String string,int flag){
         char[] chars = string.toCharArray();
         for (int i = 0;i <string.length();i++){
             if(string.charAt(i)>=65&&string.charAt(i)<=67){
-                chars[i] = (char) (string.charAt(i)+23);
+                chars[i] = (char) (string.charAt(i)+26-flag);
             }
             else if(string.charAt(i)>=97&&string.charAt(i)<=99){
-                chars[i] = (char)(string.charAt(i)+23);
+                chars[i] = (char)(string.charAt(i)+26-flag);
 
             }
             else {
--- 0527/CaesarCipher/src/club/banyuan/Encode.java
+++ 0527/CaesarCipher/src/club/banyuan/Encode.java
@@ -6,10 +6,10 @@ public class Encode {
         char[] ch = string.toCharArray();
         for (int i = 0;i< string.length();i++){
             if (string.charAt(i)>=88&&string.charAt(i)<=90){
-                ch[i]= (char) (string.charAt(i)- 23);
+                ch[i]= (char) (string.charAt(i)- 26 + flag);
             }
             else if(string.charAt(i)>=120&&string.charAt(i)<=122){
-                ch[i]= (char) (string.charAt(i)-23);
+                ch[i]= (char) (string.charAt(i)-26+flag);
             }
             else {
                 ch[i] = (char) (string.charAt(i)+flag);
