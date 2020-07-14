7b5a1e235913d947a52885135bde065fc5c08c1b
athos9494 <1026060156@qq.com>
2020-07-14 23:25:35 +0800

    .

--- java EE/oasystem/src/main/java/club/banyuan/dao/LotsDao.java
+++ java EE/oasystem/src/main/java/club/banyuan/dao/LotsDao.java
@@ -5,9 +5,7 @@ import club.banyuan.entity.Lots;
 import java.util.List;
 
 public interface LotsDao {
-    public List<Lots> getLotsByOpinion(String sql)throws Exception;
-    public Lots getLotsById(Integer id)throws Exception;
-    public List<Lots> getLotsBySell(Integer isSelled) throws Exception;
+    public List<Lots> getLots();
     public int updateLots(Lots Lots) throws Exception;
     public int addLots(Lots Lots) throws Exception;
     public int deleteLots(Integer id) throws  Exception;
--- java EE/oasystem/src/main/java/club/banyuan/dao/UserDao.java
+++ java EE/oasystem/src/main/java/club/banyuan/dao/UserDao.java
@@ -6,6 +6,5 @@ import java.util.List;
 
 public interface UserDao {
     public int add(User user);
-    public List<User> getLoginUser(User user);
-    public int getRegistUser(String userName,String userPwd,Integer idNum,String mobile,String address,String postCode);
+    public User getLoginUser(User user);
 }
